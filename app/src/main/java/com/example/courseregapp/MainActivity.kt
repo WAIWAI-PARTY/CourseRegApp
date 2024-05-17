fc1b3e549882d4ca02b1d64ef9712b3794e10515
WAIWAI-PARTY <96366002+WAIWAI-PARTY@users.noreply.github.com>
2024-05-17 12:50:58 +0800

    work in progress

--- app/src/main/java/com/example/courseregapp/MainActivity.kt
+++ app/src/main/java/com/example/courseregapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.courseregapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -16,6 +18,29 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        println("HelloWorld")
+        getBtn()
+
+    }
+    private fun getBtn(){
+        val regCourseBtn = findViewById<CardView>(R.id.reg_course_btn)
+        regCourseBtn.setOnClickListener {
+            changePage(RegCourseActivity::class.java)
+        }
+        val myProfileBtn = findViewById<CardView>(R.id.my_profile_btn)
+        myProfileBtn.setOnClickListener {
+            changePage(MyProfileActivity::class.java)
+        }
+        val scheduleBtn = findViewById<CardView>(R.id.schedule_btn)
+        scheduleBtn.setOnClickListener {
+            changePage(ScheduleActivity::class.java)
+        }
+        val courseFindBtn = findViewById<CardView>(R.id.course_find_btn)
+        courseFindBtn.setOnClickListener {
+            changePage(CourseFindActivity::class.java)
+        }
+    }
+    private fun changePage(activity: Class<*>){
+        val intent = Intent(this, activity)
+        startActivity(intent)
     }
 }
